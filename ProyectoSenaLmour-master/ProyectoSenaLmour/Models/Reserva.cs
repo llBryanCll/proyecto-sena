3f0796097fe3f5980d59031fd196c7114d62d6c6
Onichan7 <Mfer.mejia76@gmail.com>
2024-04-09 02:45:19 -0500

    Reservas
    Organicé nombres

--- ProyectoSenaLmour-master/ProyectoSenaLmour/Models/Reserva.cs
+++ ProyectoSenaLmour-master/ProyectoSenaLmour/Models/Reserva.cs
@@ -1,20 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace ProyectoSenaLmour.Models;
 
 public partial class Reserva
 {
+    [DisplayName("Id")]
     public int IdReserva { get; set; }
 
+    [DisplayName("Documento Cliente")]
     public int? NroDocumentoCliente { get; set; }
 
+    [DisplayName("Documento Usuario")]
     public int? NroDocumentoUsuario { get; set; }
 
+    [DisplayName("Fecha de reserva")]
     public DateTime? FechaReserva { get; set; }
 
+    [DisplayName("Fecha de Ingreso")]
     public DateTime? FechaInicio { get; set; }
 
+    [DisplayName("Fecha de Salida")]
     public DateTime? FechaFinalizacion { get; set; }
 
     public double? SubTotal { get; set; }
@@ -23,12 +30,16 @@ public partial class Reserva
 
     public double? Iva { get; set; }
 
+    [DisplayName("Monto Total")]
     public double? MontoTotal { get; set; }
 
+    [DisplayName("Método de Pago")]
     public int? IdMetodoPago { get; set; }
 
+    [DisplayName("Número de Personas")]
     public int? NroPersonas { get; set; }
 
+    [DisplayName("Estado de Reserva")]
     public int? IdEstadoReserva { get; set; }
 
     public virtual ICollection<Abono> Abonos { get; set; } = new List<Abono>();
@@ -37,11 +48,15 @@ public partial class Reserva
 
     public virtual ICollection<DetalleReservaServicio> DetalleReservaServicios { get; set; } = new List<DetalleReservaServicio>();
 
+    [DisplayName("Estado de Reserva")]
     public virtual EstadosReserva? IdEstadoReservaNavigation { get; set; }
 
+    [DisplayName("Método de Pago")]
     public virtual MetodoPago? IdMetodoPagoNavigation { get; set; }
 
+    [DisplayName("Documento Cliente")]
     public virtual Cliente? NroDocumentoClienteNavigation { get; set; }
 
+    [DisplayName("Documento Usuario")]
     public virtual Usuario? NroDocumentoUsuarioNavigation { get; set; }
 }
