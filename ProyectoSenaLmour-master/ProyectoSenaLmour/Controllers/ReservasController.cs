ba81d497145472f755ccabba1aedf62e86d69bf8
GRUPOSENA <152796603+KEVINDBP@users.noreply.github.com>
2024-04-09 19:26:16 -0500

    arregle lo de usuario

--- ProyectoSenaLmour-master/ProyectoSenaLmour/Controllers/ReservasController.cs
+++ ProyectoSenaLmour-master/ProyectoSenaLmour/Controllers/ReservasController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,9 @@ using ProyectoSenaLmour.Models.ViewModels;
 
 namespace ProyectoSenaLmour.Controllers
 {
+
+
+  
     public class ReservasController : Controller
     {
         private readonly LmourContext _context;
